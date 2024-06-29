5c0e8ce7bc5b336855ccb2b158a830a50df020ee
Beza Tameru <bbt0002@auburn.edu>
2024-06-29 17:57:44 -0500

    Initial Commit

---
+++ simple_clock_app2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simple_clock_app2
+//
+//  Created by Bezawit Tameru on 6/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
